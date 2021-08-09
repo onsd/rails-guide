34bc23d40d7cf58122994f0729e726228cdb90d5
Takamichi Omori <merrytakamerry@gmail.com>
2021-08-09 21:50:22 +0900

    fix migrate for sqlite

--- book_admin/db/migrate/20210410035516_add_publisher_id_to_books.rb
+++ book_admin/db/migrate/20210410035516_add_publisher_id_to_books.rb
@@ -1,5 +1,6 @@
 class AddPublisherIdToBooks < ActiveRecord::Migration[6.1]
   def change
-    add_reference :books, :publisher, null: false, foreign_key: true
+    add_reference :books, :publisher, { foreign_key: true }
+    change_column :books, :publisher_id, :integer, null: false
   end
 end
